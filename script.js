8b7c176c3eba2a153e04d3abc91964e1ef028c99
maxelonej <maxelone.py@gmail.com>
2023-11-18 00:15:07 +0400

    Add awards able to popup full img

--- script.js
+++ script.js
@@ -61,3 +61,27 @@ document.addEventListener('DOMContentLoaded', () => {
         mapChoice3.style = 'display: block;';
     });
 });
+
+// awards full img
+var fullImgBox = document.getElementById('fullImgBox');
+var fullImg = document.getElementById('fullImg');
+
+const openFullImg = (img) => {
+    fullImgBox.style.display = 'flex';
+    fullImg.src = img;
+}
+
+const closeFullImg = () => {
+    fullImgBox.style.display = 'none';
+}
+
+// Закрытие всплывающего окна через ESC
+document.addEventListener('keydown', event => {
+    if (event.which === 27) {
+        closeFullImg();
+    }
+});
+
+fullImgBox.addEventListener("click", (event) => {
+    fullImgBox.style.display = 'none';
+});
