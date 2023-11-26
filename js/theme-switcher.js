24884fa2f93fa8fdff0d9d9d92c0d833c88dec8f
maxelonej <maxelone.py@gmail.com>
2023-11-26 05:06:17 +0400

    Make map dark for dark theme

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,10 +1,21 @@
 var desktopSwitcher = document.getElementById('theme-toggle');
 var mobileSwitcher = document.getElementById('dropdown-theme-toggle')
+const iframes = document.querySelectorAll('.iframe iframe')
 
 desktopSwitcher.onclick = () => {
+    // Включение темной темы
     document.body.classList.toggle('dark-theme');
+    
+    // Включение ночной карты
+    iframes.forEach((iframe, index) => {
+        iframe.classList.toggle('night-map')
+    })
 }
 
+// То же самое, только для другого input id, который находится в hamburger menu 
 mobileSwitcher.onclick = () => {
 	document.body.classList.toggle('dark-theme')
+    iframes.forEach((iframe, index) => {
+        iframe.classList.toggle('night-map')
+    })
 }
