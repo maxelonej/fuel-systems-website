9cc8447058afa5ef1b7535ee84edef41e397da6f
maxelonej <maxelone.py@gmail.com>
2023-12-03 15:07:42 +0400

    Update main hamburger menu

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,8 +1,11 @@
 let path = window.location.pathname;
 let page = path.split("/").pop();
 
+const iframes = document.querySelectorAll('.iframe iframe');
 let light = document.getElementById('light');
 let dark = document.getElementById('dark');
+let mobileLight = document.getElementById('mobileLight');
+let mobileDark = document.getElementById('mobileDark');
 
 light.onclick = () => {
     document.body.classList.add('dark-theme');
@@ -14,11 +17,21 @@ light.onclick = () => {
         });
     }
 }
+mobileLight.onclick = () => {
+    document.body.classList.add('dark-theme');
+    mobileLight.style.display = 'none';
+    mobileDark.style.display = 'block';
+    if (page === 'index.html') {
+        iframes.forEach((iframe, index) => {
+            iframe.classList.toggle('night-map');
+        });
+    }
+}
 
-dark.onclick = () => {
+mobileDark.onclick = () => {
     document.body.classList.remove('dark-theme');
-    light.style.display = 'block';
-    dark.style.display = 'none';
+    mobileLight.style.display = 'block';
+    mobileDark.style.display = 'none';
     if (page === 'index.html') {
         iframes.forEach((iframe, index) => {
             iframe.classList.toggle('night-map');
