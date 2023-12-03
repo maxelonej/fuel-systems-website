149779c059d189fdc99fc72a4de569fd2d200d53
maxelonej <maxelone.py@gmail.com>
2023-12-03 16:28:04 +0400

    Realize .more

---
+++ js/show-more.js
@@ -0,0 +1,15 @@
+const showMore = document.querySelector('.more');
+const vacanciesLength = document.querySelectorAll('.vacancies-item').length;
+let items = 2;
+
+showMore.addEventListener('click', () => {
+    items += 2;
+    const arr = Array.from(document.querySelector('.vacancies-container').children);
+    const visibleItems = arr.slice(0, items);
+
+    visibleItems.forEach(element => element.classList.add('is-visible'));
+
+    if (visibleItems.length === vacanciesLength) {
+        showMore.style.display = 'none';
+    }
+});
